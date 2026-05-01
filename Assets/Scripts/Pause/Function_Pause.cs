b11cdfcaf27343f1f9684ca4d3ade9ace28b48dc
yoswarit0123-maker <yoswarit0123@gmail.com>
2026-05-02 04:15:59 +0700

    -

--- Assets/Scripts/Pause/Function_Pause.cs
+++ Assets/Scripts/Pause/Function_Pause.cs
@@ -1,15 +1,15 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Function_Pause : MonoBehaviour
 {
     public void OnRestart()
     {
         Time.timeScale = 1;
-        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void OnMainMenu()
     {
         Time.timeScale = 1;
-        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        SceneManager.LoadScene(0);
     }
 }
